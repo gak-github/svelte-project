2205db888f09e3b686cd81de803a5341c85d028b
Asokumar Gurusamy <Asokumar Gurusamy>
2020-06-15 17:21:04 -0700

    svelte project sample

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h2>Hello {name}!</h2>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h2 {
+		color: #1c4591;
+		/*text-transform: uppercase; */
+		font-size: 2em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
