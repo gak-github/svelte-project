9f256499e6c6042878ea5bade22c8acf4d3ee01d
Asokumar Gurusamy <Asokumar Gurusamy>
2020-06-17 18:36:51 -0700

    serverless integration

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,5 +4,5 @@
     let github = './images/github-32px.png';
 </script>
 
-<h3 className='top-head'> A full stack web application using react hooks and context api<a href="https://github.com/gak-github/svelte-project" target="_blank" rel="noopener noreferrer"><img src={github} alt="github url" /></a></h3>
+<h3 className='top-head'> A full stack web application using svelte/express/mongodb using serverless-http<a href="https://github.com/gak-github/svelte-project" target="_blank" rel="noopener noreferrer"><img src={github} alt="github url" /></a></h3>
 <h2>Expense Tracker</h2>
